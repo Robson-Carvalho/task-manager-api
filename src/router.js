bba68c6afca0246e85029537f2a91527584f8858
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-07-15 10:54:11 -0300

    feat: rotas do crud tarefa adicionandas

--- src/router.js
+++ src/router.js
@@ -1,16 +1,28 @@
 const Router = require("express");
 
-const authMiddleware = require("./utils/authMiddleware");
+const authMiddleware = require("./auth/authMiddleware");
 
 const router = Router();
 
 const home = require("./controllers/home");
-const register = require("./controllers/register");
-const login = require("./controllers/login");
+const register = require("./controllers/users/register");
+const login = require("./controllers/users/login");
+const createTask = require("./controllers/tasks/createTask");
+const readTask = require("./controllers/tasks/readTask");
+const deleteTask = require("./controllers/tasks/deleteTask");
+const updateTask = require("./controllers/tasks/updateTask");
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.post("/task/create", authMiddleware, createTask);
+
+router.get("/task/read", authMiddleware, readTask);
+
+router.delete("/task/delete", authMiddleware, deleteTask);
+
+router.put("/task/update", authMiddleware, updateTask);
 
 router.get("*", home);
 
