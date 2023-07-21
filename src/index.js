fff84daf1d879a41a34f40855446fb490ae2bcc0
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-07-21 19:07:49 -0300

    feat: cors adicionando e configurado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const swaggerUI = require("swagger-ui-express");
-const router = require("./router");
-const port = process.env.PORT || 3030;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const router = require("./router");
 const swaggerDocs = require("./swagger.json");
+
+const port = process.env.PORT || 3030;
+const allowOrigin = process.ALLOW_ORIGIN;
+
+const corsOptions = {
+  origin: allowOrigin,
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
 app.use(router);
 
 app.listen(port, () => {
