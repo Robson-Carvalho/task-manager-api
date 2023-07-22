e2951e035a672fd5b29d3e57f1b98baf707685d4
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-07-21 21:27:58 -0300

    feat: documentação das rotas finalizado

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
     });
 
     res.status(200).json({
-      user: { name },
+      user: name,
       token: token,
     });
   } catch (error) {
--- src/controllers/users/register.js
+++ src/controllers/users/register.js
@@ -1,4 +1,3 @@
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const sql = require("../../database/postgres");
 
